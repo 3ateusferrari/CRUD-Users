7d0caadad4734a18536a148d457931544afe7d01
Mateus <mateusspier@gmail.com>
2025-10-24 19:17:14 -0300

    feat: implements User Model

--- src/main/java/mateusferrari/crud_users/model/User.java
+++ src/main/java/mateusferrari/crud_users/model/User.java
@@ -1,4 +1,37 @@
 package mateusferrari.crud_users.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Username cannot be empty")
+    private String username;
+
+    @Size(min = 6, message = "The password need to have at least 6 characters")
+    @JsonIgnore
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
